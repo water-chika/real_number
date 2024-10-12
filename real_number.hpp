41fe159d27c88d2a30c47ef57334267d1ddcae31
Water Chika <water_chika@outlook.com>
2024-10-12 17:39:14 +0800

    Initial commit

---
+++ real_number.hpp
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <cstdint>
+
+namespace water {
+	namespace real_number {
+		enum class operator_enum : int32_t {
+			add,
+			sub,
+			div,
+			mul,
+			sqrt,
+			neg,
+		};
+
+		class real_number {
+		public:
+		private:
+
+		};
+	}
+}
---
+++ real_number_test.cpp
@@ -0,0 +1,6 @@
+#include <real_number.hpp>
+
+int main() {
+	
+	return 0;
+}
