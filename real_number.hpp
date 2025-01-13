0507d7ed6c622fc09489f5e048dc30fca2c10565
Water Chika <water_chika@outlook.com>
2025-01-13 16:05:58 +0000

    Change real number implementation.

--- real_number.hpp
+++ real_number.hpp
@@ -5,170 +5,144 @@
 #include <variant>
 #include <iostream>
 #include <cassert>
+#include <vector>
 
-#include <rational_number.hpp>
+namespace real_number {
+    using offset_t = size_t;
+    class int64 {
+    public:
+        int64() = default;
+        int64(int64_t v) : m_v{v} {}
+        auto value() {
+            return m_v;
+        }
+    private:
+        int64_t m_v;
+    };
+    std::ostream& operator<<(std::ostream& out, int64 v) {
+        return out << v.value();
+    }
+    template<char OP>
+    class binary_expr {
+    public:
+        binary_expr() = default;
+        binary_expr(offset_t arg0, offset_t arg1) : m_args{arg0, arg1} {}
+        auto arg_offset(int index) {
+            return m_args[index];
+        }
+    private:
+        offset_t m_args[2];
+    };
+    class sqrt_expr {
+    public:
+        sqrt_expr() = default;
+        sqrt_expr(offset_t arg) : m_arg{arg} {}
+    private:
+        offset_t m_arg;
+    };
 
-namespace water {
-	namespace real_number {
-		/*struct rational_number {
-			rational_number() : rational_number{ 0 } {}
-			rational_number(std::integral auto i) : x{  static_cast<double>(i) } {}
-			rational_number(std::floating_point auto f) : x{ f } {}
-			double x;
-		};*/
-		using rational_number = water::rational_number::rational_number<int64_t, int64_t>;
-		struct real_number;
-		struct sqrt_expr{
-			sqrt_expr();
-			sqrt_expr(const real_number& r);
-			sqrt_expr(const sqrt_expr& e);
-			sqrt_expr& operator=(const sqrt_expr& e);
-			~sqrt_expr() {
-				delete expr;
-			}
-			real_number* expr;
-		};
-		template<char OP>
-		struct binary_expr {
-			binary_expr() = delete;
-			binary_expr(const real_number& l, const real_number& r);
-			binary_expr(const binary_expr& e);
-			binary_expr& operator=(const binary_expr& e);
-			~binary_expr() {
-				delete left;
-				delete right;
-			}
-			real_number* left;
-			real_number* right;
-		};
+    class real_number {
+    public:
+        using expr_variant = 
+            std::variant<
+                int64,
+                binary_expr<'+'>,
+                binary_expr<'-'>,
+                binary_expr<'*'>,
+                binary_expr<'/'>,
+                sqrt_expr
+                >;
+        real_number(int64_t v) : m_expr{int64{v}} {}
+        real_number() : real_number(0) {}
+        real_number(expr_variant expr) : m_expr{expr} {}
+        template<char OP>
+        real_number(binary_expr<OP> expr, real_number arg0, real_number arg1)
+            : m_expr(1+arg0.m_expr.size()+arg1.m_expr.size()) {
+                m_expr[0] = typeof(expr){0, arg0.m_expr.size()};
+                std::ranges::copy(arg0.m_expr, m_expr.begin()+1);
+                std::ranges::copy(arg1.m_expr, m_expr.begin()+1+arg0.m_expr.size());
+            }
+        real_number(sqrt_expr expr, real_number arg)
+            : m_expr(1 + arg.m_expr.size()) {
+                m_expr[0] = typeof(expr){0};
+                std::ranges::copy(arg.m_expr, m_expr.begin() + 1);
+            }
 
-		struct real_number {
-			real_number() : real_number{ 0.0 } {}
-			real_number(std::integral auto i) : real_number{ rational_number{i} } {}
-			real_number(std::floating_point auto i) : real_number{ rational_number{i} } {}
-			real_number(const rational_number& q) : exp{ q } {}
-			std::variant<rational_number,
-				sqrt_expr,
-				binary_expr<'+'>,
-				binary_expr<'-'>,
-				binary_expr<'*'>,
-				binary_expr<'/'>> exp;
-		};
+        auto size() const {
+            return static_cast<offset_t>(m_expr.size());
+        }
+        auto& operator[](size_t index) const {
+            return m_expr[index];
+        }
+        auto begin() const {
+            return m_expr.begin();
+        }
+        auto end() const {
+            return m_expr.end();
+        }
+    private:
+        static_assert(std::is_copy_assignable_v<expr_variant>
+                && std::is_copy_constructible_v<expr_variant>
+                );
+        std::vector<
+            expr_variant,
+            std::allocator<expr_variant>
+            > m_expr;
+    };
 
-		inline std::ostream& operator<<(std::ostream& out, real_number r) {
-			if (std::holds_alternative<rational_number>(r.exp)) {
-				out << std::get<rational_number>(r.exp);
-			}
-			else if (std::holds_alternative<sqrt_expr>(r.exp)) {
-				out << "sqrt(" << *std::get<sqrt_expr>(r.exp).expr << ")";
-			}
-			else if (std::holds_alternative<binary_expr<'+'>>(r.exp)) {
-				out << *std::get<binary_expr<'+'>>(r.exp).left << "+" << *std::get<binary_expr<'+'>>(r.exp).right;
-			}
-			else if (std::holds_alternative<binary_expr<'-'>>(r.exp)) {
-				out << '(' << * std::get<binary_expr<'-'>>(r.exp).left << ')'
-					<< "-" 
-					<< '(' << * std::get<binary_expr<'-'>>(r.exp).right << ')';
-			}
-			else if (std::holds_alternative<binary_expr<'*'>>(r.exp)) {
-				out << '(' << * std::get<binary_expr<'*'>>(r.exp).left << ')'
-					<< "*"
-					<< '(' << *std::get<binary_expr<'*'>>(r.exp).right << ')';
-			}
-			else if (std::holds_alternative<binary_expr<'/'>>(r.exp)) {
-				out << '(' << * std::get<binary_expr<'/'>>(r.exp).left << ')'
-					<< "/"
-					<< '(' << * std::get<binary_expr<'/'>>(r.exp).right << ')';
-			}
-			else {
-				assert(0);
-			}
-			return out;
-		}
+    real_number operator+(const real_number& lhs, const real_number& rhs) {
+        return real_number{binary_expr<'+'>{}, lhs, rhs};
+    }
+    real_number operator-(const real_number& lhs, const real_number& rhs) {
+        return real_number{binary_expr<'-'>{}, lhs, rhs};
+    }
+    real_number operator*(const real_number& lhs, const real_number& rhs) {
+        return real_number{binary_expr<'*'>{}, lhs, rhs};
+    }
+    real_number operator/(const real_number& lhs, const real_number& rhs) {
+        return real_number{binary_expr<'/'>{}, lhs, rhs};
+    }
+    real_number sqrt(const real_number& v) {
+        return real_number{sqrt_expr{}, v};
+    }
 
-		inline real_number operator+(real_number lhs, real_number rhs) {
-			real_number res{};
-			if (std::holds_alternative<rational_number>(lhs.exp) &&
-				std::holds_alternative<rational_number>(rhs.exp)) {
-				res.exp = rational_number{ std::get<rational_number>(lhs.exp) + std::get<rational_number>(rhs.exp) };
-			}
-			else {
-				res.exp = binary_expr<'+'>{lhs, rhs};
-			}
-			return res;
-		}
-		inline real_number operator+=(real_number& lhs, const real_number& rhs) {
-			lhs = lhs + rhs;
-			return lhs;
-		}
-		inline real_number operator-(real_number lhs, real_number rhs) {
-			real_number res{};
-			if (std::holds_alternative<rational_number>(lhs.exp) &&
-				std::holds_alternative<rational_number>(rhs.exp)) {
-				res.exp = rational_number{ std::get<rational_number>(lhs.exp) - std::get<rational_number>(rhs.exp) };
-			}
-			else {
-				res.exp = binary_expr<'-'>{ lhs, rhs };
-			}
-			return res;
-		}
-		inline real_number operator-=(real_number& lhs, const real_number& rhs) {
-			lhs = lhs - rhs;
-			return lhs;
-		}
-		inline real_number operator/(real_number lhs, real_number rhs) {
-			real_number res{};
-			if (std::holds_alternative<rational_number>(lhs.exp) &&
-				std::holds_alternative<rational_number>(rhs.exp)) {
-				res.exp = rational_number{ std::get<rational_number>(lhs.exp) / std::get<rational_number>(rhs.exp) };
-			}
-			else {
-				res.exp = binary_expr<'/'>{ lhs, rhs };
-			}
-			return res;
-		}
-		inline real_number operator/=(real_number& lhs, const real_number& rhs) {
-			lhs = lhs / rhs;
-			return lhs;
-		}
-		inline real_number operator*(real_number lhs, real_number rhs) {
-			real_number res{};
-			if (std::holds_alternative<rational_number>(lhs.exp) &&
-				std::holds_alternative<rational_number>(rhs.exp)) {
-				res.exp = rational_number{ std::get<rational_number>(lhs.exp) * std::get<rational_number>(rhs.exp) };
-			}
-			else {
-				res.exp = binary_expr<'*'>{ lhs, rhs };
-			}
-			return res;
-		}
-		inline real_number operator*=(real_number& lhs, const real_number& rhs) {
-			lhs = lhs * rhs;
-			return lhs;
-		}
-		inline real_number sqrt(real_number r) {
-			real_number res{};
-			res.exp = sqrt_expr{ r };
-			return res;
-		}
-		template<char OP>
-		inline binary_expr<OP>::binary_expr(const real_number& l, const real_number& r)
-			: left{new real_number}, right{new real_number}
-		{
-			*left = l;
-			*right = r;
-		}
-		template<char OP>
-		inline binary_expr<OP>::binary_expr(const binary_expr& e)
-			: binary_expr{ *e.left, *e.right }
-		{
-		}
-		template<char OP>
-		inline binary_expr<OP>& binary_expr<OP>::operator=(const binary_expr& e)
-		{
-			*left = *e.left;
-			*right = *e.right;
-			return *this;
-		}
-	}
+    // deduce
+
+    // output
+    std::ostream& output(std::ostream& out, int64 v, auto begin, auto end) {
+        return out << v;
+    }
+    std::ostream& output(std::ostream& out, sqrt_expr expr, auto begin, auto end) {
+        out << "sqrt(";
+        output(out, *begin, begin+1, end);
+        out << ")";
+        return out;
+    }
+    template<char OP>
+    std::ostream& output(std::ostream& out, binary_expr<OP> expr, auto begin, auto end) {
+        out << "(";
+        output(out, *begin, begin+1, begin + expr.arg_offset(1));
+        out << ")";
+        out << OP;
+        out << "(";
+        output (out, *(begin+expr.arg_offset(1)), begin+expr.arg_offset(1)+1, end);
+        out << ")";
+        return out;
+    }
+    std::ostream& output(std::ostream& out, real_number::expr_variant expr, auto begin, auto end) {
+        return std::visit(
+                [&out, &begin, &end](auto& t) -> auto& {
+                    return output(out, t, begin, end);
+                },
+                expr
+                );
+    }
+
+    std::ostream& operator<<(std::ostream& out, const real_number& v) {
+        return std::visit(
+                [&out, &v](auto& t) -> auto&{
+                    return output(out, t, v.begin()+1, v.end());
+                },
+                v[0]);
+    }
 }
--- real_number_test.cpp
+++ real_number_test.cpp
@@ -1,13 +1,13 @@
 #include <real_number.hpp>
 
 int main() {
-	water::real_number::real_number r{};
+	real_number::real_number r{};
 	r = r + 2;
 	r = r / 3;
 	r = sqrt(r);
 	r = r + 2;
 	r = r / 3;
-	r = r * sqrt(water::real_number::real_number{ 2 });
-	std::cout << r << std::endl;
+	r = r * sqrt(real_number::real_number{ 2 });
+    std::cout << r << std::endl;
 	return 0;
 }
